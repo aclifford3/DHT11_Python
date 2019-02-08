8b5aa8764c6395d369e79806692a0d2a75af15fb
aclifford3 <alec.clifford@gmail.com>
2019-02-08 18:51:48 -0500

    Flattened directories

--- get_temp_humidity.py
+++ get_temp_humidity.py
@@ -1,5 +1,4 @@
 import RPi.GPIO as GPIO
-import time
 import dht11
 from slushie_client import SlushieClient
 import config
@@ -10,15 +9,13 @@ GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
 # read data using pin 25
-instance = dht11.dht11.DHT11(pin = 25)
+instance = dht11.DHT11(pin=25)
 result = instance.read()
 
 client = SlushieClient()
-while(True):
-    if result.is_valid():
-        print("Temperature: %d C" % result.temperature)
-        print("Humidity: %d %%" % result.humidity)
-        client.put_weather_data(config.DEVICE_ID, temperature=result.temperature, humidity=result.humidity)
-    else:
-        print("Error: %d" % result.error_code)
-    time.sleep(1)
+if result.is_valid():
+    print("Temperature: %d C" % result.temperature)
+    print("Humidity: %d %%" % result.humidity)
+    client.put_weather_data(config.DEVICE_ID, temperature=result.temperature, humidity=result.humidity)
+else:
+    print("Error: %d" % result.error_code)
