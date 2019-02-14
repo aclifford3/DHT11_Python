1022a91a8252ef2ff7c166766aea7d58409b0b95
aclifford3 <alec.clifford@gmail.com>
2019-02-13 19:59:06 -0500

    Added hello world test

---
+++ tests/test_get_temp_humidity.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestGetTempHumidity(unittest.TestCase):
+
+    def test_hello_world(self):
+        self.assertAlmostEquals("hello", "hello")
+
+
+if __name__ == '__main__':
+    unittest.main()
